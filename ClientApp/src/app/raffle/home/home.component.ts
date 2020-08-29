5766f718c69159ea5470806b9a2dfeda9d0139b2
D0683497 <ff129043923@gmail.com>
2020-08-30 07:23:36 +0800

    完善展示抽獎活動頁面

--- ClientApp/src/app/raffle/home/home.component.ts
+++ ClientApp/src/app/raffle/home/home.component.ts
@@ -1,6 +1,13 @@
 import { RaffleService } from './../../services/raffle.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Round } from 'src/app/models/round/round.model';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { shareReplay, map } from 'rxjs/operators';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { AddComponent } from '../add/add.component';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +16,41 @@ import { Round } from 'src/app/models/round/round.model';
 })
 export class HomeComponent implements OnInit {
 
-  dataSource: Round[];
-  displayedColumns: string[] = ['id', 'name', 'complete'];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  constructor(private raffleService: RaffleService) { }
+  dataSource = new MatTableDataSource<Round>();
+  displayedColumns: string[] = ['name', 'complete', 'action'];
+  fetchDataError = true;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver, private raffleService: RaffleService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.raffleService.getRounds()
       .subscribe(
         data => {
-          this.dataSource = data;
+          this.dataSource.data = data;
+          this.dataSource.paginator = this.paginator;
+          this.fetchDataError = false;
         },
-        error => {}
+        error => {
+          console.warn(error.status);
+          this.fetchDataError = true;
+        }
       );
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
