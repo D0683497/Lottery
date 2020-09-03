71618025e38a39c5dc1c2c91525140c4664794e8
D0683497 <ff129043923@gmail.com>
2020-09-03 11:01:30 +0800

    新增ViewModel

---
+++ Models/Attendee/AttendeeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lottery.Models.Attendee
+{
+    public class AttendeeViewModel
+    {
+        [Display(Name = "參與者識別碼")]
+        public string Id { get; set; }
+        
+        [Display(Name = "參與者學號")]
+        public string NID { get; set; }
+        
+        [Display(Name = "參與者姓名")]
+        public string Name { get; set; }
+
+        [Display(Name = "參與者系所")]
+        public string Department { get; set; }
+    }
+}
---
+++ Models/Item/ItemAddViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lottery.Models.Item
+{
+    public class ItemAddViewModel
+    {
+        [Required(ErrorMessage = "{0}是必填的")]
+        [Display(Name = "清單名稱")]
+        public string Name { get; set; }
+    }
+}
