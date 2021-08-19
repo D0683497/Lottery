e0a9d65a0a42eba5e862315532acf7d7890a2d7f
D0683497 <ff129043923@gmail.com>
2021-08-20 05:27:10 +0800

    修改首頁及錯誤頁面、移除隱私權頁面、新增申請使用頁面及原始碼頁面

--- Lottery/Controllers/HomeController.cs
+++ Lottery/Controllers/HomeController.cs
@@ -1,33 +1,75 @@
-﻿using Lottery.Models;
+﻿using System.Diagnostics;
+using Lottery.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Lottery.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View();
         }
+        
+        [HttpGet("background")]
+        public IActionResult Background()
+        {
+            return File(System.IO.File.OpenRead("wwwroot/images/home.jpg"), "image/jpeg", $"home.jpg");
+        }
 
+        [HttpGet("privacy")]
         public IActionResult Privacy()
+        {
+            return Redirect("https://www.fcu.edu.tw/privacy/");
+        }
+        
+        [HttpGet("facebook")]
+        public IActionResult Facebook()
+        {
+            return Redirect("https://www.facebook.com/fcussc/");
+        }
+        
+        [HttpGet("instagram")]
+        public IActionResult Instagram()
+        {
+            return Redirect("https://www.instagram.com/fcu.cdc/");
+        }
+        
+        [HttpGet("github")]
+        public IActionResult GitHub()
+        {
+            return Redirect("https://github.com/fcu-ssc/");
+        }
+        
+        [HttpGet("site")]
+        public IActionResult Site()
+        {
+            return Redirect("https://ssc.fcu.edu.tw/");
+        }
+
+        [HttpGet("credits")]
+        public IActionResult Credits()
+        {
+            return View();
+        }
+        
+        [HttpGet("apply")]
+        public IActionResult Apply()
         {
             return View();
         }
 
+        [HttpGet("errors")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
